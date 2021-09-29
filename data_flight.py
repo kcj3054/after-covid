f5f33531a8f26cb2042c1d3a2a7009007cf338c9
kcj3054 <kcj3054@naver.com>
2021-09-29 13:54:32 +0900

    적재 몽고디비

--- data_flight.py
+++ data_flight.py
@@ -4,7 +4,7 @@ from urllib.request import Request, urlopen
 
 import requests
 from bs4 import BeautifulSoup
-
+import re
 url = 'http://openapi.airport.co.kr/service/rest/AirportCodeList/getAirportCodeList'
 queryParams = '?' + 'ServiceKey=' +'tczAARILXCELP0k8%2F%2FVvXLud8T8G2f%2FxL4eYeBd4EH2cREYEKcOJo0VcLL5eSJVAphD7%2BEOgW3FVLEHBebBp8w%3D%3D'
 
@@ -17,3 +17,29 @@ items = soup.select('items > item > cityKor')
 # soup = BeautifulSoup(res.text, "lxml")
 for item in items:
     print(item.text)
+
+
+# ca?e
+# care, cafe, case, cave.. 사고가 나서 기억이 안난다
+
+p = re.compile("ca.e")
+p = re.compile("$원")
+
+#. : 하나의 문자를 의미 , ^ : 문자열의 시작
+# $(se$) : 문자열의 끝 > case, base(0)
+# p.match("case")
+
+def print_match(m):
+    m = p.match("caffe")
+
+    if m:
+        print(m.group())  # 일치하는 문자열 반환
+        print(m.string) #입력받은 문자열
+        print(m.start()) # 일치하는 문자열의 시작 index
+        print(m.end()) # 일치하는 문자열의 끝 인덱스
+
+    else:
+        print("매칭되지 않음")
+
+m = p.search("good care")   # search :  주어진 문자열 중에 일치하는게 있는지 확인
+m = p.findall("careless") #findall : 일치하는 모든 것을 리스트형태로 반환
